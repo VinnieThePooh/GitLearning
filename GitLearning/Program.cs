2c57612506ef4207d14fdfeedb19f5525eba3ce6
Lashkevich Ryan <lashkevichrain@gmail.com>
2017-02-12 15:06:41 +0700

    Initial commit

---
+++ GitLearning/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.EnterpriseServices.Internal;
+using System.Globalization;
+
+namespace GitLearning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now.ToString(@"dddd, MMMM M\t\h yyyy", CultureInfo.CreateSpecificCulture("en-US")));
+            Console.ReadKey(true);
+        }
+    }
+}
